ea9aef6c069aeec9f410e8f47aa9ed9da714d2b4
Artem <dradinc@yandex.ru>
2021-03-16 16:40:39 +0300

    Edit

--- app/src/main/java/com/example/worldcinema/LaunchScreen.kt
+++ app/src/main/java/com/example/worldcinema/LaunchScreen.kt
@@ -1,11 +1,20 @@
 package com.example.worldcinema
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LaunchScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
+
+        val startApp = Intent(this, SignIn::class.java)
+        Handler().postDelayed({
+            startActivity(startApp)
+            finish()
+        }, 2000)
+
     }
 }
--- app/src/main/java/com/example/worldcinema/SignIn.kt
+++ app/src/main/java/com/example/worldcinema/SignIn.kt
@@ -2,10 +2,19 @@ package com.example.worldcinema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+    }
+
+    public fun reg_onClick (view : View) {
+        val regActivity = 
+    }
+
+    public fun auth_onClick (view : View) {
+
     }
 }
